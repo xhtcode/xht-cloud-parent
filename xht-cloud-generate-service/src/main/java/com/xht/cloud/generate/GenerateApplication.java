1a9d1f343f636e6e24d27a439bd0dba637202a75
lihongbin <616326125@qq.com>
2023-12-23 18:26:58 +0800

    feat: 代码生成器引入nacos

--- xht-cloud-generate-service/src/main/java/com/xht/cloud/generate/GenerateApplication.java
+++ xht-cloud-generate-service/src/main/java/com/xht/cloud/generate/GenerateApplication.java
@@ -1,7 +1,10 @@
 package com.xht.cloud.generate;
 
+import com.xht.cloud.framework.security.annotaion.EnableResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 描述 ：代码生成器
@@ -9,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author : 小糊涂
  * @version : 1.0
  **/
+@EnableResourceServer
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class GenerateApplication {
     public static void main(String[] args) {
